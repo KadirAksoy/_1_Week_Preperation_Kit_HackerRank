8b711353690ebbc8838727c85b5040d0d2fa0258
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-05-21 12:27:19 +0300

    Mock Test 2 solved

---
+++ src/_02_day/MockTest2.java
@@ -0,0 +1,23 @@
+package _02_day;
+
+import java.util.List;
+
+public class MockTest2 {
+    public static int flippingMatrix(List<List<Integer>> matrix) {
+        int sum = 0;
+        int size = matrix.size();
+
+        for(int i = 0; i < size/2; i++){
+            for(int j = 0; j < size/2; j++){
+                sum += Math.max(matrix.get(i).get(j),
+                        Math.max( matrix.get(i).get(size-1-j),
+                                Math.max( matrix.get(size-1-i).get(j),
+                                        matrix.get(size-1-i).get(size-1-j))
+                        )
+                );
+
+            }
+        }
+        return sum;
+    }
+}
